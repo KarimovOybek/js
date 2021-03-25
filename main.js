3b4f6686b1e8351be18b189628553a13367592b6
Oybek <punchmanone15@gmail.com>
2021-03-25 19:27:26 +0500

    javascript

---
+++ main.js
@@ -0,0 +1,45 @@
+'use strict'
+
+
+var userName = prompt('Type your name :');
+alert('Hello ' + userName + '!');
+console.log('Your name is ' + userName);
+
+
+
+var userAge = prompt('Print your age :');
+alert('Your age in ' + userAge);
+console.log('Your age in ' + userAge);
+
+var question1 = prompt('100 + 200 = ?');
+alert(question1);
+
+var answer1 = 100 + 200;
+console.log('Answer: ' + answer1 + (', Your answer is:' + ' ' + question1));
+
+var question2 = prompt('3002 - 2200 - 123 = ?');
+alert(question2);
+
+var answer2 = 3002 - 2200 - 123;
+console.log('Answer: ' + answer2 + (', Your answer is:' + ' ' + question2));
+
+var question3 = prompt('23 * 123 * 20 = ?');
+alert(question3);
+
+var answer3 = 23 * 123 * 20;
+console.log('Answer: ' + answer3 + (', Your answer is:' + ' ' + question3));
+
+var question4 = prompt('23 / 123 / 20 = ?');
+alert(question4);
+
+var answer4 = 23 / 123 / 20;
+console.log('Answer: ' + answer4 + (', Your answer is:' + ' ' + question4));
+
+var question5 = prompt('23 % 123 = ?');
+alert(question5);
+
+var answer5 = 23 % 123;
+console.log('Answer: ' + answer5 + (', Your answer is:' + ' ' + question5));
+
+var console = 'Enter to console to know your questions answer'
+alert(console)
